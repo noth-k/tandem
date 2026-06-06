15d3da161b738e6f067e71c39bb75005c42e47ce
Jian Cheng <lowjiancheng3773@gmail.com>
2026-06-06 15:30:43 +0800

    fix shit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,14 @@
 import { Router } from 'express'
 
 import { agentsRouter } from './agents.js'
-<<<<<<< HEAD
 import { debriefRouter } from './debrief.js'
-=======
-import { audioRouter } from './audio.js'
->>>>>>> 61a6761 (audio parser 1.0)
 import { demoRouter } from './demo.js'
 import { realtimeRouter } from './realtime.js'
 
 export const apiRouter = Router()
 
 apiRouter.use('/agents', agentsRouter)
-<<<<<<< HEAD
-apiRouter.use('/debrief', debriefRouter)
-=======
 apiRouter.use('/audio', audioRouter)
->>>>>>> 61a6761 (audio parser 1.0)
+apiRouter.use('/debrief', debriefRouter)
 apiRouter.use('/demo', demoRouter)
 apiRouter.use('/realtime', realtimeRouter)
